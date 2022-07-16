43b0e7d556bdf3be5e352b7dc42f504f6c222722
kwakminjung <kwak1126mj@gmail.com>
2022-07-16 22:50:06 +0900

    index수정: 넘기는 효과 넣음
    overflow: hidden이 안 먹음

--- static/home.js
+++ static/home.js
@@ -1,3 +1,11 @@
-function toBlack(this) {
-   
-}
+document.querySelector('#btn2').addEventListener('click', function(){
+    document.querySelector('.container').style.transform = 'translate(-100vw)';
+});
+
+document.querySelector('#btn3').addEventListener('click', function(){
+    document.querySelector('.container').style.transform = 'translate(-200vw)';
+});
+
+document.querySelector('#btn1').addEventListener('click', function(){
+    document.querySelector('.container').style.transform = 'translate(0vw)';
+});
