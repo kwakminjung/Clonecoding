aa80a455cf31dbc6707858bc70baaf5f8cffdc25
kwakminjung <kwak1126mj@gmail.com>
2022-07-17 23:25:32 +0900

    js 움직일 거리 수정

--- static/home.js
+++ static/home.js
@@ -1,9 +1,9 @@
 document.querySelector('#btn2').addEventListener('click', function(){
-    document.querySelector('.container').style.transform = 'translate(-85vw)';
+    document.querySelector('.container').style.transform = 'translate(-84vw)';
 });
 
 document.querySelector('#btn3').addEventListener('click', function(){
-    document.querySelector('.container').style.transform = 'translate(-170vw)';
+    document.querySelector('.container').style.transform = 'translate(-168vw)';
 });
 
 document.querySelector('#btn1').addEventListener('click', function(){
