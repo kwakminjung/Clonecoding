0985445966c05d04c3d9e1a4407f355261140b1f
KwakMJ <kwak1126mj@gmail.com>
2022-07-18 21:37:25 +0900

    Update home.js

--- static/home.js
+++ static/home.js
@@ -21,9 +21,6 @@ document.querySelector('#btn1').addEventListener('click', function(){
 
 document.querySelector('#nextBtn').addEventListener('click', function(){
     var Glist = document.querySelector('.group');
-    //만약 화면에 Glist[1] 보이면 -84vw 만큼 이동
-    //만약 화면에 Glist[2] 보이면 -84*2vw 만큼 이동
-    //btn(아래 단추)와 같이 움직여야 하는 점 감안
     document.querySelector('.container').style.transform = 'translate(-84vw)';
 
     document.querySelector('#btn2').src = "http://127.0.0.1:5500/carrot/static/images/blackcircle.jpg";
