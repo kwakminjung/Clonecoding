b9fccc2509eb8aeb777f4efd118b060f6da7d96c
kwakminjung <kwak1126mj@gmail.com>
2022-07-23 15:53:52 +0900

    IR.html 완성

---
+++ static/IR.js
@@ -0,0 +1,7 @@
+document.querySelector('#disclosure').addEventListener('click', function(){
+    document.querySelector('.IRLsit').style.transform = 'translate(0vw)';
+});
+
+document.querySelector('#FinancialStatements').addEventListener('click', function(){
+    document.querySelector('.IRLsit').style.transform = 'translate(-100vw)';
+});
--- static/home.js
+++ static/home.js
@@ -29,7 +29,7 @@ document.querySelector('#nextBtn').addEventListener('click', function(){
     document.querySelector('#btn2').src = "http://127.0.0.1:5500/carrot/static/images/blackcircle.jpg";
     document.querySelector('#btn1').src = "http://127.0.0.1:5500/carrot/static/images/circle.jpg";
     document.querySelector('#btn3').src = "http://127.0.0.1:5500/carrot/static/images/circle.jpg";
-})
+});
 
 document.querySelector('#beforeBtn').addEventListener('click', function(){
     document.querySelector('.container').style.transform = 'translate(0vw)';
@@ -37,4 +37,4 @@ document.querySelector('#beforeBtn').addEventListener('click', function(){
     document.querySelector('#btn1').src = "http://127.0.0.1:5500/carrot/static/images/blackcircle.jpg";
     document.querySelector('#btn2').src = "http://127.0.0.1:5500/carrot/static/images/circle.jpg";
     document.querySelector('#btn3').src = "http://127.0.0.1:5500/carrot/static/images/circle.jpg";
-})
+});
