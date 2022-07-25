90e0f1f3c9d7e981e84eef8a36b01ea3ef3845f4
kwakminjung <kwak1126mj@gmail.com>
2022-07-25 20:52:07 +0900

    culture 마진 수정

---
+++ static/culture.js
@@ -0,0 +1,15 @@
+document.querySelector('#H1').addEventListener('click', function(){
+    document.querySelector('.WorkTV').style.transform = 'translate(0vw)';
+});
+
+document.querySelector('#H2').addEventListener('click', function(){
+    document.querySelector('.WorkTV').style.transform = 'translate(-84vw)';
+});
+
+document.querySelector('#H3').addEventListener('click', function(){
+    document.querySelector('.WorkTV').style.transform = 'translate(-168vw)';
+});
+
+document.querySelector('#H4').addEventListener('click', function(){
+    document.querySelector('.WorkTV').style.transform = 'translate(-252vw)';
+});
