4b03b9308548f208036f587cd45118fe624674d5
kwakminjung <kwak1126mj@gmail.com>
2022-07-31 17:36:56 +0900

    index 슬라이드 수정

--- static/culture.js
+++ static/culture.js
@@ -1,15 +1,39 @@
-document.querySelector('#H1').addEventListener('click', function(){
-    document.querySelector('.WorkTV').style.transform = 'translate(0vw)';
-});
+// document.querySelector('#H1').addEventListener('click', function(){
+//     document.querySelector('.WorkTV').style.transform = 'translate(0vw)';
+// });
 
-document.querySelector('#H2').addEventListener('click', function(){
-    document.querySelector('.WorkTV').style.transform = 'translate(-84vw)';
-});
+// document.querySelector('#H2').addEventListener('click', function(){
+//     document.querySelector('.WorkTV').style.transform = 'translate(-84vw)';
+// });
 
-document.querySelector('#H3').addEventListener('click', function(){
-    document.querySelector('.WorkTV').style.transform = 'translate(-168vw)';
-});
+// document.querySelector('#H3').addEventListener('click', function(){
+//     document.querySelector('.WorkTV').style.transform = 'translate(-168vw)';
+// });
 
-document.querySelector('#H4').addEventListener('click', function(){
-    document.querySelector('.WorkTV').style.transform = 'translate(-252vw)';
-});
+// document.querySelector('#H4').addEventListener('click', function(){
+//     document.querySelector('.WorkTV').style.transform = 'translate(-252vw)';
+// });
+
+
+let workPage = document.querySelectorAll(".H");
+
+function workClick(event) {
+
+  if (event.target.classList[0] === "selected") {
+    workPage[i].classList.add("selected");
+  } else {
+    for (let i = 0; i < workPage.length; i++) {
+      page[i].classList.add("unselected");
+    }
+
+    event.target.classList.remove("selected");
+  }
+}
+
+function border() {
+  for (let i = 0; i < workPage.length; i++) {
+    page[i].addEventListener('click', workClick);
+  }
+}
+
+border();
---
+++ static/main.js
@@ -0,0 +1,47 @@
+let mem_menu = document.querySelectorAll('.button .btn');
+
+let slides = document.querySelector('.container');
+let slide_li = document.querySelectorAll('.group');
+let currentIdx = 0; // 현재 위치
+let slideCount = slide_li.length;
+const prevButton = document.querySelector('.before #leftBtn');
+const nextButton = document.querySelector('.after #rightBtn');
+
+function moveSlide(num) {
+  slides.style.left = (-num * 79 + 'vw');
+  currentIdx = num;
+}
+
+nextButton.addEventListener('click', function() {
+  if (currentIdx < slideCount - 1) {
+      moveSlide(currentIdx + 1);
+  } else {
+      moveSlide(0);
+  }
+});
+
+prevButton.addEventListener('click', function() {
+  if (currentIdx > 0) {
+      moveSlide(currentIdx - 1);
+  } else {
+      moveSlide(slideCount - 2);
+  }
+});
+
+function setStyle_m(num) {
+  for (var i = 0; i < mem_menu.length; i++) {
+      if (i == num) {
+          mem_menu[i].classList.remove('menu_unselected');
+          mem_menu[i].classList.add('menu_selected');
+      } else {
+          mem_menu[i].classList.remove('menu_selected');
+          mem_menu[i].classList.add('menu_unselected');
+      }
+  }
+  
+  moveSlide(0);
+}
+
+window.onload = function() {
+  setStyle_m();
+}
