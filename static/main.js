3dc7239a58e35ca2b4af654f0d23660aa29171be
kwakminjung <kwak1126mj@gmail.com>
2022-07-31 19:11:36 +0900

    main.js 슬라이드 수정

--- static/main.js
+++ static/main.js
@@ -1,5 +1,3 @@
-let mem_menu = document.querySelectorAll('.button .btn');
-
 let slides = document.querySelector('.container');
 let slide_li = document.querySelectorAll('.group');
 let currentIdx = 0; // 현재 위치
@@ -8,8 +6,9 @@ const prevButton = document.querySelector('.before #leftBtn');
 const nextButton = document.querySelector('.after #rightBtn');
 
 function moveSlide(num) {
-  slides.style.left = (-num * 79 + 'vw');
+  slides.style.left = (-num * 97 + '%');
   currentIdx = num;
+  setStyle_m(num);
 }
 
 nextButton.addEventListener('click', function() {
@@ -24,9 +23,11 @@ prevButton.addEventListener('click', function() {
   if (currentIdx > 0) {
       moveSlide(currentIdx - 1);
   } else {
-      moveSlide(slideCount - 2);
+      moveSlide(slideCount - 1);
   }
 });
+
+let mem_menu = document.querySelectorAll('#button .btn');
 
 function setStyle_m(num) {
   for (var i = 0; i < mem_menu.length; i++) {
@@ -39,9 +40,9 @@ function setStyle_m(num) {
       }
   }
   
-  moveSlide(0);
+  moveSlide(num);
 }
 
 window.onload = function() {
-  setStyle_m();
+  setStyle_m(currentIdx);
 }
