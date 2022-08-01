77339cb831e3cd49b4f7ed113a4641c48b2bb5de
kwakminjung <kwak1126mj@gmail.com>
2022-08-01 22:19:07 +0900

    팀 문화 슬라이드 수정

--- static/culture.js
+++ static/culture.js
@@ -1,9 +1,12 @@
 let btn = document.querySelectorAll('.HTT .H');
+// let cont = document.querySelector('.WorkTV');
 
 btn[0].addEventListener('click', function(){
-    document.querySelector('.WorkTV').style.transform = 'translate(0vw)';
+    document.querySelector('.WorkTV').style.transform = 'translate(0%)';
+    // cont.classList.replace('Htrans1');
     for(let i=0; i<4; i++) {
         if(i==0) {
+            // cont.classList.replace('Htrans1');
             btn[i].classList.add('selected');
             btn[i].classList.remove('unselected');
         } else {
@@ -14,9 +17,11 @@ btn[0].addEventListener('click', function(){
 });
 
 btn[1].addEventListener('click', function(){
-    document.querySelector('.WorkTV').style.transform = 'translate(-81.5vw)';
+    document.querySelector('.WorkTV').style.transform = 'translate(-100%)';
+    // cont.classList.replace('Htrans2');
     for(let i=0; i<4; i++) {
         if(i==1) {
+            // cont.classList.replace('Htrans2');
             btn[i].classList.add('selected');
             btn[i].classList.remove('unselected');
         } else {
@@ -27,9 +32,11 @@ btn[1].addEventListener('click', function(){
 });
 
 btn[2].addEventListener('click', function(){
-    document.querySelector('.WorkTV').style.transform = 'translate(-163vw)';
+    document.querySelector('.WorkTV').style.transform = 'translate(-200%)';
+    // cont.classList.replace('Htrans3');
     for(let i=0; i<4; i++) {
         if(i==2) {
+            // cont.classList.replace('Htrans3');
             btn[i].classList.add('selected');
             btn[i].classList.remove('unselected');
         } else {
@@ -40,7 +47,8 @@ btn[2].addEventListener('click', function(){
 });
 
 btn[3].addEventListener('click', function(){
-    document.querySelector('.WorkTV').style.transform = 'translate(-245vw)';
+    document.querySelector('.WorkTV').style.transform = 'translate(-300%)';
+    // cont.classList.replace('Htrans4');
     for(let i=0; i<4; i++) {
         if(i==3) {
             btn[i].classList.add('selected');
